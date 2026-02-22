705eb64cedfb04c978648d825683760ed9c407a3
ZWE04237 <zwe04237@educastur.es>
2026-02-22 22:35:02 +0100

    Reorganize SQL script for database creation

--- Ejercicios guiados/Entradas.sql
+++ Ejercicios guiados/Entradas.sql
@@ -1,8 +1,35 @@
 create database entradas;
 use entradas;
 
+create table recintos(
+    cod_recinto int,
+    nombre varchar(10) not null,
+    direccion varchar(20) not null,
+    ciudad varchar(10) not null,
+    telefono numeric(9),
+    horario varchar(10),
+    constraint pk_recintos primary key (cod_recinto)
+);
+
+create table espectadores(
+    dni_cliente varchar(9),
+    nombre varchar (10),
+    direccion varchar(20),
+    ciudad varchar(10),
+    telefono numeric(9),
+    ntarjeta int not null,
+    constraint pk_espectadores primary key (dni_cliente)
+);
+
+create table zonas_recintos(
+    cod_recinto int,
+    zona varchar(15) not null,
+    capacidad int not null,
+    constraint pk_zonas_recintos primary key (cod_recinto, zona)
+);
+
 create table espectaculos(
-    cod_espectaculo int;
+    cod_espectaculo int,
     nombre varchar(10) not null,
     tipo varchar(15),
     fecha_inicial date not null,
@@ -11,6 +38,14 @@ create table espectaculos(
     cod_recinto int,
     constraint pk_espectaculos primary key (cod_espectaculo),
     constraint fk_espectaculos_cod_recinto foreign key (cod_recinto) references recintos (cod_recinto)
+);
+
+create table representaciones(
+    cod_espectaculo int,
+    fecha date not null,
+    hora varchar(10),
+    constraint pk_representaciones primary key (cod_espectaculo, fecha, hora),
+    constraint fk_representaciones_cod_espectaculo foreign key (cod_espectaculo) references espectaculos (cod_espectaculo)
 );
 
 create table precios_espectaculos(
@@ -23,37 +58,12 @@ create table precios_espectaculos(
     constraint fk_precios_espectaculos_zonas_recintos foreign key (cod_recinto, zona) references zonas_recintos(cod_recinto,zona)
 );
 
-create table recintos(
-    cod_recinto int,
-    nombre varchar(10) not null,
-    direccion varchar(20) not null,
-    ciudad varchar(10) not null,
-    telefono numeric(9),
-    horario varchar(10),
-    constraint pk_recintos primary key (cod_recintos)
-);
-
-create table zonas_recintos(
-    cod_recinto int,
-    zona varchar(15) not null,
-    capacidad int not null,
-    constraint pk_zonas_recintos primary key (cod_recinto, zona)
-);
-
 create table asientos(
     cod_recinto int,
     zona varchar(15),
     fila int,
     numero int,
     constraint pk_asientos primary key (cod_recinto, zona, fila, numero)
-);
-
-create table representaciones(
-    cod_espectaculo int,
-    fecha date not null,
-    hora varchar(10),
-    constraint pk_representaciones primary key (cod_espectaculo, fecha, hora),
-    constraint fk_representaciones_cod_espectaculo foreign key (cod_espectaculo) references espectaculos (cod_espectaculo)
 );
 
 create table entradas(
@@ -71,12 +81,3 @@ create table entradas(
     constraint fk_entradas_asiento foreign key (cod_recinto, zona, fila, numero) references asientos (cod_recinto, zona, fila, numero)
 );
 
-create table espectadores(
-    dni_cliente varchar(9),
-    nombre varchar (10),
-    direccion varchar(20),
-    ciudad varchar(10),
-    telefono numeric(9),
-    ntarjeta int not null,
-    constraint pk_espectadores primary key (dni_cliente)
-);
