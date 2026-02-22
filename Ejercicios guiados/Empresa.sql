6167974c6e8b12252c905ae53249ac44941a726c
ZWE04237 <zwe04237@educastur.es>
2026-02-22 22:34:37 +0100

    Recreate database and tables with corrections

--- Ejercicios guiados/Empresa.sql
+++ Ejercicios guiados/Empresa.sql
@@ -1,6 +1,39 @@
 create database empresa;
 use empresa;
 
+create table empleados(
+    dni varchar(9),
+    nombre varchar(10) not null,
+    apellido1 varchar(10) not null,
+    apellido2 varchar(10),
+    direcc1 varchar(20),
+    direcc2 varchar(20),
+    ciudad varchar(20),
+    cod_postal decimal(5,0),
+    sexo char(1),
+    fecha_nac date,
+    constraint pk_empleados primary key (dni),
+    constraint ck_empleados_sexo check (sexo = 'H' or sexo = 'M') -- !!!
+);
+
+create table universidades(
+    univ_cod int,
+    nombre_univ varchar(20) not null,
+    ciudad varchar(20),
+    municipio varchar(20),
+    cod_postal decimal(5,0),
+    constraint pk_universidades primary key (univ_cod)
+);
+
+create table trabajos(
+    trabajo_cod int,
+    nombre_trab varchar(20) not null,
+    salario_min decimal(6,2) not null,
+    salario_max decimal(6,2) not null,
+    constraint pk_trabajos primary key (trabajo_cod),
+    constraint uq_trabajos_nombre unique (nombre_trab)
+);
+
 create table departamentos(
     dpto_cod int,
     nombre_dpto varchar(10) not null,
@@ -8,8 +41,8 @@ create table departamentos(
     presupuesto int not null,
     pres_actual int,
     constraint pk_departamentos primary key (dpto_cod),
-    constraint uq_departamentos_nombre unique (nombre_departamentos),
-    constraint fk_departamentos_dpto_padre foreign key (dpto_pdare) references departamentos(dpto_cod)
+    constraint uq_departamentos_nombre unique (nombre_dpto),
+    constraint fk_departamentos_dpto_padre foreign key (dpto_padre) references departamentos(dpto_cod)
 );
 
 create table estudios(
@@ -46,35 +79,7 @@ create table historial_salarial( -- En la corrección no aparece, supongo que es
     constraint fk_historial_salarial_empleado_dni foreign key (empleado_dni) references empleados(dni)
 );
 
-create table empleados(
-    dni varchar(9),
-    nombre varchar(10) not null,
-    apellido1 varchar(10) not null,
-    apellido2 varchar(10),
-    direcc1 varchar(20),
-    direcc2 varchar(20),
-    ciudad varchar(20),
-    cod_postal decimal(5,0),
-    sexo char,
-    fecha_nac date,
-    constraint pk_empleados primary key (dni),
-    constraint ck_empleados_sexo check (sexo = 'H' or sexo = 'M') -- !!!
-);
 
-create table universidades(
-    univ_cod int,
-    nombre_univ varchar(20) not null,
-    ciudad varchar(20),
-    municipio varchar(20),
-    cod_postal decimal(5,0),
-    constraint pk_universidades primary key (univ_cod),
-    );
 
-create table trabajos(
-    trabajo_cod int,
-    nombre_trab varchar(20) not null,
-    salario_min decimal(6,2) not null,
-    salario_max decimal(6,2) not null,
-    constraint pk_trabajos primary key (trabajo_cod),
-    constraint uq_trabajos_nombre unique (nombre_trab)
-);
+
+
